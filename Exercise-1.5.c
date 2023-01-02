e6fecf6987031c63f69db50285a1e06413d0ac19
Roger Almengor González <roger.almengor@gaf.de>
2023-01-02 15:15:37 +0100

    comparison

--- Exercise-1.5.c
+++ Exercise-1.5.c
@@ -7,7 +7,7 @@ int main()
     printf("Fahrenheit to Celsius conversion:\n"); 
     printf("---------------------------------\n");
 
-    for (fahr = 300; fahr <= 0; fahr = fahr - 20)
+    for (fahr = 300; fahr > 0; fahr = fahr - 20)
     {
         printf("%3.1f %6.1f\n", fahr, ((fahr -32) *5) /9);
     }
