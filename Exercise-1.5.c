48e561d79e104c83847f687142bc359d5a012101
Roger Almengor González <roger.almengor@gaf.de>
2023-01-02 14:47:30 +0100

    exercise 5

---
+++ Exercise-1.5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+    float fahr, celsius; 
+    int lower, upper, step;
+
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size */ 
+    
+    printf("Fahrenheit to Celsius conversion:\n"); 
+    printf("---------------------------------\n");
+
+    fahr = upper; 
+
+    for (fahr = 0, fahr == lower, fahr = fahr + step){
+        celsius = (celsius * 1.8) + 32; 
+        printf("%3d %6.1f\n", fahr, celsius);
+    }
+
+    return 0; 
+}
